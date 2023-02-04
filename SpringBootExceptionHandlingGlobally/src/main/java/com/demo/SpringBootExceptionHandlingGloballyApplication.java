444c4656894d04f471f800faa5005b4f467cdbc6
unknown <you@example.com>
2023-02-04 17:02:10 +0530

    added logs in main class

--- SpringBootExceptionHandlingGlobally/src/main/java/com/demo/SpringBootExceptionHandlingGloballyApplication.java
+++ SpringBootExceptionHandlingGlobally/src/main/java/com/demo/SpringBootExceptionHandlingGloballyApplication.java
@@ -3,10 +3,14 @@ package com.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class SpringBootExceptionHandlingGloballyApplication {
+import lombok.extern.slf4j.Slf4j;
 
+@SpringBootApplication
+@Slf4j
+public class SpringBootExceptionHandlingGloballyApplication {
+	
 	public static void main(String[] args) {
+		log.info("loggers added ");
 		SpringApplication.run(SpringBootExceptionHandlingGloballyApplication.class, args);
 	}
 
