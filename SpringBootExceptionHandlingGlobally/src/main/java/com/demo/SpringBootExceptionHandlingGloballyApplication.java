9ca55b94d54af95205e8c1fe7e5b680aa8af1041
unknown <you@example.com>
2023-02-04 17:44:13 +0530

    Update SpringBootExceptionHandlingGloballyApplication.java

--- SpringBootExceptionHandlingGlobally/src/main/java/com/demo/SpringBootExceptionHandlingGloballyApplication.java
+++ SpringBootExceptionHandlingGlobally/src/main/java/com/demo/SpringBootExceptionHandlingGloballyApplication.java
@@ -10,7 +10,6 @@ import lombok.extern.slf4j.Slf4j;
 public class SpringBootExceptionHandlingGloballyApplication {
 	
 	public static void main(String[] args) {
-		log.info("loggers added");
 		SpringApplication.run(SpringBootExceptionHandlingGloballyApplication.class, args);
 	}
 
